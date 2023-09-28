012a296fd23f3ea3697aad0873eb104428c18c5d
portfolioilham <codesportfolio@gmail.com>
2023-09-28 18:53:04 +0700

    first commit

---
+++ JS/index.js
@@ -0,0 +1,33 @@
+
+
+
+function frontendweb() {
+
+  const background=document.getElementById("macbook");
+  background.setAttribute("src", "./image/laptop2.jpg");
+  background.style.marginTop="50px";
+
+  
+  
+}
+function frontendweb2() {
+
+  const background=document.getElementById("macbook");
+  background.setAttribute("src", "./image/macbookpro.jpg");
+  background.style.marginTop="50px";
+
+  
+  
+}
+function frontendweb3() {
+
+  const background=document.getElementById("macbook");
+  background.setAttribute("src", "./image/macbookpro2.jpg");
+  background.style.marginTop="50px";
+
+  
+  
+}
+
+
+
